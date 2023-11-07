7ad1a61706271234a119aca27bfc9fc7b54c5c04
Calvin <CalvinIvan@live.com>
2023-11-07 16:48:18 -0500

    changes to UI

--- src/app/Navbar/nav.tsx
+++ src/app/Navbar/nav.tsx
@@ -1,9 +1,7 @@
 "use client";
 import React, { useState } from "react";
-import next from "@/public/next.svg";
 import { navLinks } from "@/app/lib/data";
 import Link from "next/link";
-import Image from "next/image";
 import { CgMenuGridO } from "react-icons/cg";
 
 const Nav = () => {
@@ -15,10 +13,10 @@ const Nav = () => {
     <div>
       <CgMenuGridO
         onClick={handleNav}
-        className="fixed right-4 top-4 z-[99] overflow-hidden text-4xl hover:cursor-pointer lg:hidden"
+        className="fixed right-4 top-4 z-[99] overflow-hidden text-4xl text-white hover:cursor-pointer lg:hidden"
       />
       {nav ? (
-        <div className="fixed top-0 z-20 flex h-screen w-full flex-col items-center justify-center bg-white/90 duration-200 ease-in-out ">
+        <div className="fixed top-0 z-20 flex h-screen w-full flex-col items-center justify-center bg-white/[0.05] duration-200 ease-in-out ">
           <ul className="flex flex-col items-center justify-center">
             {navLinks.map((link) => {
               return (
@@ -36,19 +34,20 @@ const Nav = () => {
         </div>
       ) : (
         <div className="relative z-[999] hidden lg:block">
-          <nav className="fixed top-0 flex w-full justify-between bg-gradient-to-r from-rose-400/90 to-red-500/90 py-4 sm:h-[initial]">
-            <ul className="flex flex-row items-center justify-center gap-[2.5rem] px-16 text-xl">
+          <nav className="fixed left-1/2 top-2 flex w-[50%] -translate-x-1/2 items-center justify-center rounded-full bg-gray-200/[0.25] py-4 sm:h-[initial]">
+            <ul className="flex flex-row items-center justify-center gap-[2.5rem] border border-black px-16 text-xl">
               {navLinks.map((link) => {
                 return (
                   <li
                     key={link.name}
-                    className=" font-medium text-white transition hover:scale-105 hover:underline"
+                    className="font-normal text-white transition hover:scale-105 hover:underline"
                   >
                     <Link href={link.link}>{link.name}</Link>
                   </li>
                 );
               })}
             </ul>
+            <div>asdfasdf</div>
           </nav>
         </div>
       )}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
 import Image from "next/image";
-
+import Background from "@/assets/bg.png";
 export default function Home() {
-  return;
+  return (
+    <>
+      <div className="max-h-screen overflow-hidden">
+        <Image
+          src={Background}
+          alt="Background"
+          width={2000}
+          className="blur-[2.5rem]"
+          priority
+        />
+      </div>
+      <div></div>
+    </>
+  );
 }
