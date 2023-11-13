fe9d1be4db28648115270a86cb2d24bd4c4df84c
Calvin <CalvinIvan@live.com>
2023-11-13 17:45:59 -0500

    =-/

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,31 +1,31 @@
-import { prisma } from "@/lib/prisma";
-import ProductCard from "../components/ProductCard";
+"use client";
 
-interface ProductProps {
-  category: string;
-}
-
-export async function products({ category }: ProductProps) {
-  const products = await prisma.product.findMany({
-    where: {
-      category: {
-        equals: category,
-      },
-    },
-  });
-  return (
-    <div className="my-4 grid w-[95%] grid-cols-1  gap-2 md:ml-0 lg:mx-16 lg:grid-cols-2 xl:mx-2 xl:grid-cols-3">
-      {products.slice().map((product) => (
-        <ProductCard product={product} key={product.id} />
-      ))}
-    </div>
-  );
-}
+import { useEffect, useState } from "react";
+import { products } from "@/lib/GetSelection";
 
 export default function Menu() {
+  const [selection, setSelection] = useState("");
+
+  useEffect(() => {
+    products({ category: selection });
+  }, [selection]);
+
   return (
     <section className="mt-16 flex w-[95%] flex-col">
-      {products({ category: "Burger" })}
+      <h1>Select Category:</h1>
+      <div className="mb-3 rounded-xl bg-white/75 p-2">
+        <label className="font-semibold text-gray-700/90">Category: </label>
+        <select
+          name="category"
+          id="category"
+          className="rounded-xl bg-white/75 p-2 text-gray-700/90"
+          onChange={(e) => setSelection(e.target.value)}
+        >
+          <option value="Pizza">Pizza</option>
+          <option value="Burger">Burger</option>
+        </select>
+      </div>
+      {selection}
     </section>
   );
 }
--- src/lib/GetSelection.tsx
+++ src/lib/GetSelection.tsx
@@ -1,34 +1,23 @@
-"use client";
+import ProductCard from "@/app/components/ProductCard";
+import { prisma } from "./prisma";
 
-import { useState } from "react";
+interface ProductProps {
+  category: string;
+}
 
-export default function GetSelection() {
-  // State to manage the selected option
-  const [selectedOption, setSelectedOption] = useState<string | undefined>(
-    undefined,
-  );
-
-  // Function to handle select change
-  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedOption(event.target.value);
-  };
-
+export async function products({ category }: ProductProps) {
+  const products = await prisma.product.findMany({
+    where: {
+      category: {
+        equals: category,
+      },
+    },
+  });
   return (
-    <section>
-      <h1>Select Category:</h1>
-      <div className="mb-3 rounded-xl bg-white/75 p-2">
-        <label className="font-semibold text-gray-700/90">Category: </label>
-        <select
-          onChange={handleSelectChange}
-          name="category"
-          id="category"
-          className="rounded-xl bg-white/75 p-2 text-gray-700/90"
-        >
-          <option value="Pizza">Pizza</option>
-          <option value="Burger">Burger</option>
-        </select>
-      </div>
-      <p>Selected Option: {selectedOption || "None"}</p>
-    </section>
+    <div className="my-4 grid w-[95%] grid-cols-1  gap-2 md:ml-0 lg:mx-16 lg:grid-cols-2 xl:mx-2 xl:grid-cols-3">
+      {products.slice().map((product) => (
+        <ProductCard product={product} key={product.id} />
+      ))}
+    </div>
   );
 }
